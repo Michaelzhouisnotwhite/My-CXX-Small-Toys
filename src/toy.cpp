cf451091f2d6145196a6f8ffc19eb3026594e0f8
Michaelzhouisnotwhite <zyqing601@163.com>
2023-08-22 22:41:45 +0800

    udpate

--- src/toy.cpp
+++ src/toy.cpp
@@ -1,24 +1,29 @@
-#include "toy/pch.hpp"
 #include "toy/include.h"
+#include "toy/pch.hpp"
+
+#ifdef PATH_MAX
+#undef PATH_MAX
 #define PATH_MAX 1024
+#endif
 std::filesystem::path toy::GetExePath() {
 #ifdef _WIN32// 如果是Windows平台
-        char buffer[MAX_PATH] = {0};
-        GetModuleFileNameA(nullptr, buffer, MAX_PATH);
+    char buffer[MAX_PATH] = {0};
+    GetModuleFileNameA(nullptr, buffer, MAX_PATH);
 #else// 如果是Linux平台
-        char buffer[PATH_MAX] = {0};
-        ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX);
-        if (count < 0 || count >= PATH_MAX) return "";
+    char buffer[PATH_MAX] = {0};
+    ssize_t count = readlink("/proc/self/exe", buffer, PATH_MAX);
+    if (count < 0 || count >= PATH_MAX) return "";
 #endif
-        return std::filesystem::path(buffer).parent_path();
+    return std::filesystem::path(buffer).parent_path();
 }
 
-std::uint64_t toy::get_core_nums()  {
+std::uint64_t toy::get_core_nums() {
 #ifdef _WIN32
-        SYSTEM_INFO sys_info;
-        GetSystemInfo(&sys_info);
-        return sys_info.dwNumberOfProcessors;
+    SYSTEM_INFO sys_info;
+    GetSystemInfo(&sys_info);
+    return sys_info.dwNumberOfProcessors;
 #else
-        return get_nprocs();
+    return get_nprocs();
 #endif
-    }
+}
+#undef PATH_MAX
