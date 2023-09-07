ce8410a0139c4be0c19c57fb922269db9146be07
Michaelzhouisnotwhite <zyqing601@163.com>
2023-09-07 13:39:20 +0800

    add rust type

---
+++ include/toy/type.h
@@ -0,0 +1,29 @@
+#ifndef INCLUDE_TOY_TYPE
+#define INCLUDE_TOY_TYPE
+#include <deque>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#define TYPE1 template<typename T>
+#define TYPE2 template<typename T, typename A>
+TYPE1
+using Vec = std::vector<T>;
+TYPE1
+using VecDeque = std::deque<T>;
+TYPE1
+using LinkedList = std::list<T>;
+TYPE2
+using BTreeMap = std::map<T, A>;
+TYPE2
+using HashMap = std::unordered_map<T, A>;
+TYPE1
+using HashSet = std::unordered_set<T>;
+TYPE1
+using BTreeSet = std::set<T>;
+TYPE1
+using BinaryHeat=std::priority_queue<T>;
+#endif /* INCLUDE_TOY_TYPE */
